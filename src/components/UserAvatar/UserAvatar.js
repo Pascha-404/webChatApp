8d41bd5b99be0f8c1b74b76765b670d47b785474
Patrick Pavliuchik <73493957+Pascha-404@users.noreply.github.com>
2022-01-08 17:36:25 +0100

    starter file

---
+++ src/components/UserAvatar/UserAvatar.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Avatar from '@mui/material/Avatar';
+
+
+function UserAvatar() {
+    return (
+        <div>
+            <Avatar />
+        </div>
+    )
+}
+
+export default UserAvatar;
