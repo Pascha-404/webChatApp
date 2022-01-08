91ff49e84a48e5559290f672bbe6035858348a91
Patrick Pavliuchik <73493957+Pascha-404@users.noreply.github.com>
2022-01-08 17:35:00 +0100

    changed icons and styling

---
+++ src/components/layout/Navbar/Navbar.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import Tabs from '@mui/material/Tabs';
+import Tab from '@mui/material/Tab';
+import HomeIcon from '@mui/icons-material/Home';
+import SendIcon from '@mui/icons-material/Send';
+import InboxIcon from '@mui/icons-material/Inbox';
+import GroupIcon from '@mui/icons-material/Group';
+import NotificationsIcon from '@mui/icons-material/Notifications';
+import MoreHorizIcon from '@mui/icons-material/MoreHoriz';
+import SettingsIcon from '@mui/icons-material/Settings';
+
+import UserAvatar from '../../UserAvatar';
+
+function Navbar() {
+	const [value, setValue] = React.useState("home");
+
+	const styles = {
+		height: '100%',
+		width: '10%',
+		backgroundColor: '#181C2F',
+		display: 'flex',
+		flexDirection: 'column',
+		alignItems: 'center',
+		justifyContent: 'space-around',
+	};
+	const handleChange = (event, newValue) => {
+		setValue(newValue);
+	};
+	return (
+		<nav style={styles}>
+			<UserAvatar />
+			<Tabs
+				indicatorColor='secondary'
+				textColor='secondary'
+				orientation='vertical'
+				value={value}
+				onChange={handleChange}
+				aria-label='icon tabs example'>
+				<Tab icon={<HomeIcon />} aria-label='Home' value={'home'} />
+				<Tab icon={<SendIcon />} aria-label='Send' value={'send'} />
+				<Tab icon={<InboxIcon />} aria-label='Inbox' value={'inbox'} />
+				<Tab icon={<GroupIcon />} aria-label='Users' value={'users'} />
+				<Tab icon={<NotificationsIcon />} aria-label='Notifications' value={'notifications'} />
+				<Tab icon={<MoreHorizIcon />} aria-label='Options' value={'options'} /><Tab icon={<SettingsIcon />} aria-label='Settings' value={'settings'} />
+			</Tabs>
+		</nav>
+	);
+}
+
+export default Navbar;
