ff99e84dbce7b253a399c8ae5aa55a5c9e89aa72
Patrick Pavliuchik <73493957+Pascha-404@users.noreply.github.com>
2022-01-10 23:37:57 +0100

    created function for randomMessages

--- src/components/Inbox/Inbox.js
+++ src/components/Inbox/Inbox.js
@@ -4,6 +4,7 @@ import { Grid } from '@mui/material';
 import { MessagesContext } from '../../contexts/messages.context';
 
 import SearchForm from '../SearchForm';
+import SortForm from '../SortForm/SortForm';
 import MessageCard from '../MessageCard';
 
 import useStyles from './Inbox.style';
@@ -11,20 +12,39 @@ import useStyles from './Inbox.style';
 // { userId: 5, date: '2021/20/3', time: '14pm', msg: 'Hey, how are you?' }
 
 function Inbox() {
-	const messages = useContext(MessagesContext);
+	const savedMessages = useContext(MessagesContext);
 	const classes = useStyles();
+
+	const createMessages = array => {
+		let createdMessages = [];
+
+		for (let i = 0; i < 15; i++) {
+			let randNmbr = Math.floor(Math.random() * array.length);
+			createdMessages.push(
+				<MessageCard
+					key={array[randNmbr].userId}
+					userId={array[randNmbr].userId}
+					msg={array[randNmbr].chatMessages[0].msg}
+					time={array[randNmbr].chatMessages[0].time}
+				/>
+			);
+		}
+		return createdMessages;
+	};
+	const messages = createMessages(savedMessages);
 	return (
 		<Grid item sm={4} className={classes.inbox}>
-			<SearchForm />
-
-			{messages.map(msg => (
+			<SearchForm className={classes.inboxSearchForm}/>
+			<SortForm />
+			<div className={classes.inboxMessages}>{messages}</div>
+			{/* {messages.map(msg => (
 				<MessageCard
 					key={msg.userId}
 					userId={msg.userId}
 					msg={msg.chatMessages[0].msg}
 					time={msg.chatMessages[0].time}
 				/>
-			))}
+			))} */}
 		</Grid>
 	);
 }
