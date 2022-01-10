80429522d4b4b9df1cbbf34339d1c3e2b8c762c2
Patrick Pavliuchik <73493957+Pascha-404@users.noreply.github.com>
2022-01-10 23:38:30 +0100

    fixed styling for scrollbar

--- src/components/Inbox/Inbox.style.js
+++ src/components/Inbox/Inbox.style.js
@@ -2,9 +2,19 @@ import { createUseStyles } from 'react-jss';
 
 const useStyles = createUseStyles({
 	inbox: {
-		background: "linear-gradient(135deg, #FFFFFF 0%, #F8F8F8 65.34%, #FFFFFF 100%)",
+		background: 'linear-gradient(135deg, #FFFFFF 0%, #F8F8F8 65.34%, #FFFFFF 100%)',
 		paddingTop: '2rem',
+		height: '100%',
+	},
+	inboxMessages: {
+		width: '100%',
+		height: '79%',
+		overflowY: 'scroll',
+		'&::-webkit-scrollbar': {
+			width: '0px',
+			height: '0px'
+		},
 	},
 });
 
-export default useStyles
+export default useStyles;
