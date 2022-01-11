1a233cfabfd58de90d3703e057f59c37c9282e7d
Patrick Pavliuchik <73493957+Pascha-404@users.noreply.github.com>
2022-01-11 19:40:44 +0100

    fixed styling for icons and erased unused code

--- src/components/layout/Navbar/Navbar.style.js
+++ src/components/layout/Navbar/Navbar.style.js
@@ -13,25 +13,22 @@ const useStyles = createUseStyles({
 		width: '100%',
 		'& .MuiTab-root': {
 			color: '#fff',
-            opacity: '0.3',
+			opacity: '0.3',
 			'&:last-of-type': {
 				marginTop: '4rem',
 			},
-			"& .MuiSvgIcon-root": { fontSize: "30px"},
+			'& .MuiSvgIcon-root': { fontSize: '30px', color: '#fff' },
 		},
-		'& .Mui-selected': { opacity: '1' },
+		'& .Mui-selected': { opacity: '1', color: '#FFF'},
 		'& .MuiTabs-indicator': {
 			right: null,
-			left: 0,
+			left: 10,
 			width: '26px',
-			
 			background: 'linear-gradient(90deg, rgba(255,0,0,1) 0%, rgba(41,47,76,0.1) 100%)',
-		},
-    },
-    menuIcon: {
-        height: '24px',
-        width: '100%'
-    }
+		}, '& .MuiButtonBase-root': {
+			paddingRight: '0',
+		}
+	},
 });
 
 export default useStyles;
