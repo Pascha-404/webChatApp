f52b68c6923d76b234d496a04ec8fbf57dd89c8f
Patrick Pavliuchik <73493957+Pascha-404@users.noreply.github.com>
2022-01-11 19:33:12 +0100

    formated code

--- src/App.js
+++ src/App.js
@@ -2,11 +2,11 @@ import WebChatApp from './components/WebChatApp';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <WebChatApp />
-    </div>
-  );
+	return (
+		<div className='App'>
+			<WebChatApp />
+		</div>
+	);
 }
 
 export default App;
