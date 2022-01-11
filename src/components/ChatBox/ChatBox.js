4c7d8962a0902d2d360efd8cd898b1fb8f4cccba
Patrick Pavliuchik <73493957+Pascha-404@users.noreply.github.com>
2022-01-12 00:28:53 +0100

    added messages from context, msgWrapper styling

--- src/components/ChatBox/ChatBox.js
+++ src/components/ChatBox/ChatBox.js
@@ -1,13 +1,25 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import { Grid } from '@mui/material';
 import ChatBoxInput from '../ChatBoxInput';
+import ChatBubble from '../ChatBubble/ChatBubble';
+
+import { MessagesContext } from '../../contexts/messages.context';
 
 import useStyles from './ChatBox.style';
 
 function ChatBox() {
 	const classes = useStyles();
+	const allMessages = useContext(MessagesContext)
+	const messages = allMessages[0].chatMessages
 	return (
 		<Grid item sm={5} className={classes.chatBox}>
+
+			<section className={classes.msgWrapper}>
+				{messages.map(msg => {
+					return <ChatBubble msg={msg.msg} isMe={msg.userId === 1 && true} time={msg.time}/>
+						})}
+			</section>
+
 			<ChatBoxInput />
 		</Grid>
 	);
--- src/components/ChatBox/ChatBox.style.js
+++ src/components/ChatBox/ChatBox.style.js
@@ -1,11 +1,22 @@
-import { createUseStyles } from "react-jss";
+import { createUseStyles } from 'react-jss';
 
 const useStyles = createUseStyles({
-    chatBox: {
-        height: '100%',
-        width: '100%',
-        position: 'relative',
+	chatBox: {
+		overflow: 'hidden',
+		height: '100%',
+		width: '100%',
+		position: 'relative',
 		backgroundColor: '#EFECE8',
+	},
+	msgWrapper: {
+		width: '93%',
+		height: '80%',
+		padding: '0 1rem',
+		overflow: 'scroll',
+		'&::-webkit-scrollbar': {
+			width: 0,
+			height: 0,
+		},
 	},
 });
 
