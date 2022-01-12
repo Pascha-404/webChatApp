e0aeb0307ba9675e3b0cf26ab81408605245ee3b
Patrick Pavliuchik <73493957+Pascha-404@users.noreply.github.com>
2022-01-12 18:28:34 +0100

    formated code, added marginBottom to chatBubbles

--- src/components/ChatBubble/ChatBubble.js
+++ src/components/ChatBubble/ChatBubble.js
@@ -2,16 +2,17 @@ import React from 'react';
 import useStyles from './ChatBubble.style';
 
 function ChatBubble(props) {
-    const classes = useStyles(props);
-    const { msg, time } = props;
+	const classes = useStyles(props);
+	const { msg, time } = props;
+
 	return (
 		<React.Fragment>
-            <div className={classes.chatBubble}>
-                <p>{msg}</p>
-                <p>{time}</p>
-                    </div>
-            <div className={classes.clear}/>
-        </React.Fragment>
+			<div className={classes.chatBubble}>
+				<p>{msg}</p>
+				<p>{time}</p>
+			</div>
+			<div className={classes.clear} />
+		</React.Fragment>
 	);
 }
 
--- src/components/ChatBubble/ChatBubble.style.js
+++ src/components/ChatBubble/ChatBubble.style.js
@@ -3,6 +3,7 @@ import { createUseStyles } from 'react-jss';
 const useStyles = createUseStyles({
 	chatBubble: {
 		position: 'relative',
+		marginBottom: '1.2rem',
 		padding: '10px 20px',
 		color: props => (props.isMe ? 'white' : 'black'),
 		background: props => (props.isMe ? '#F44A4A' : '#FFFF'),
