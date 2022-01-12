5ed7f3997fefdba0f94eb4f84887c54e13d5b98d
Patrick Pavliuchik <73493957+Pascha-404@users.noreply.github.com>
2022-01-12 18:15:21 +0100

    added more and longer fake messages

--- src/contexts/messages.context.js
+++ src/contexts/messages.context.js
@@ -6,10 +6,24 @@ const defaultMessages = [
 	{
 		userId: 1,
 		chatMessages: [
+			{ userId: 1, date: '2021/20/3', time: '12pm', msg: 'Hey, how are you?' },
+			{
+				userId: 2,
+				date: '2021/20/3',
+				time: '13pm',
+				msg: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed consequat nibh vel arcu sodales venenatis. Phasellus commodo nisl augue, quis.',
+			},
 			{ userId: 1, date: '2021/20/3', time: '14pm', msg: 'Hey, how are you?' },
-			{ userId: 2, date: '2021/20/3', time: '14pm', msg: 'Hey, how are you?' },
-			{ userId: 1, date: '2021/20/3', time: '14pm', msg: 'Hey, how are you?' },
-			{ userId: 2, date: '2021/20/3', time: '14pm', msg: 'Hey, how are you?' },
+			{ userId: 2, date: '2021/20/3', time: '15pm', msg: 'Hey, how are you?' },
+			{
+				userId: 1,
+				date: '2021/20/3',
+				time: '16pm',
+				msg: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed consequat nibh vel arcu sodales venenatis. Phasellus commodo nisl augue, quis.?',
+			},
+			{ userId: 2, date: '2021/20/3', time: '17pm', msg: 'Hey, how are you?' },
+			{ userId: 1, date: '2021/20/3', time: '18pm', msg: 'Hey, how are you?' },
+			{ userId: 2, date: '2021/20/3', time: '19pm', msg: 'Hey, how are you?' },
 		],
 	},
 	{
