f9aab5fb8fb274732d8d8cd7dc8791f03add1cf3
Patrick Pavliuchik <73493957+Pascha-404@users.noreply.github.com>
2022-01-13 01:49:39 +0100

    formated code

--- src/components/MessageCard/MessageCard.js
+++ src/components/MessageCard/MessageCard.js
@@ -1,33 +1,33 @@
 import React from 'react';
-import CardContent from '@mui/material/CardContent';
-import Typography from '@mui/material/Typography';
+import {CardContent,Card, Typography } from '@mui/material';
 import UserAvatar from '../UserAvatar';
-
 import useStyles from './MessageCard.style';
 
 // { userId: 5, date: '2021/20/3', time: '14pm', msg: 'Hey, how are you?' }
 
 function MessageCard({ userId, date, time, msg }) {
-    const classes = useStyles();
+	const classes = useStyles();
 	return (
-        <CardContent className={classes.messageCard}>
-			<div className={classes.dataBlock}>
-				<UserAvatar />
-				<div>
-					<Typography sx={{ fontSize: 18 }} color='text.secondary' gutterBottom>
-						{userId}
-					</Typography>
-					<Typography sx={{ fontSize: 14 }} color='text.secondary'>
-						Online Status
-					</Typography>
+		<Card>
+			<CardContent className={classes.messageCard}>
+				<div className={classes.dataBlock}>
+					<UserAvatar />
+					<div>
+						<Typography sx={{ fontSize: 18 }} color='text.secondary' gutterBottom>
+							{userId}
+						</Typography>
+						<Typography sx={{ fontSize: 14 }} color='text.secondary'>
+							Online Status
+						</Typography>
+					</div>
+					<Typography variant='body2'>{time}</Typography>
 				</div>
-                <Typography variant='body2'>{time}</Typography>
-			</div>
-			<div className={classes.dataBlock}>
-                <Typography variant='body2'>{msg}</Typography>
-				<Typography variant='body2'>5</Typography>
-			</div>
-		</CardContent>
+				<div className={classes.dataBlock}>
+					<Typography variant='body2'>{msg}</Typography>
+					<Typography variant='body2'>5</Typography>
+				</div>
+			</CardContent>
+		</Card>
 	);
 }
 
--- src/components/UserAvatar/UserAvatar.js
+++ src/components/UserAvatar/UserAvatar.js
@@ -1,16 +1,15 @@
-import React from 'react'
+import React from 'react';
 import Avatar from '@mui/material/Avatar';
 
 import useStyles from './UserAvatar.styles';
 
-function UserAvatar({size = 3}) {
-    const classes = useStyles({size});
-    return (
-        <div className={classes.userAvatar}>
-            <Avatar />
-        </div>
-    )
+function UserAvatar({ size = 3 }) {
+	const classes = useStyles({ size });
+	return (
+		<div className={classes.userAvatar}>
+			<Avatar />
+		</div>
+	);
 }
-
 
 export default UserAvatar;
