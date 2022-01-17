8f92d9dd47a149d02b76da695c3f93cea1aa6366
Patrick Pavliuchik <73493957+Pascha-404@users.noreply.github.com>
2022-01-17 15:55:06 +0100

    add UserContext  and logic for getting chatPartner

--- src/components/MessageCard/MessageCard.js
+++ src/components/MessageCard/MessageCard.js
@@ -1,16 +1,22 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Card, CardHeader, IconButton } from '@mui/material';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import UserAvatar from '../UserAvatar';
 import useStyles from './MessageCard.style';
 
+import { UserContext } from '../../contexts/user.context';
 
-function MessageCard({ userName, date, time, msg, imgUrl }) {
+function MessageCard({ userId, time, msg }) {
+	const {contacts} = useContext(UserContext);
+	const chatPartner = contacts.filter(contact => contact.login.uuid === userId)
 	const classes = useStyles();
+	const userName = contacts && chatPartner[0].name.first + " " + chatPartner[0].name.last
+	console.log(chatPartner);
+
 	return (
 		<Card className={classes.messageCard}>
 			<CardHeader
-				avatar={<UserAvatar userName={userName} imgUrl={imgUrl} />}
+				avatar={<UserAvatar userName={userName} imgUrl={chatPartner[0].picture.thumbnail} />}
 				title={userName}
 				subheader={msg}
 				action={
