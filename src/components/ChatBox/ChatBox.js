4c487283fdf380ab24acc55efa943a24f0d0dd0e
Patrick Pavliuchik <73493957+Pascha-404@users.noreply.github.com>
2022-01-18 17:06:30 +0100

    display content when has data or state

--- src/components/ChatBox/ChatBox.js
+++ src/components/ChatBox/ChatBox.js
@@ -6,30 +6,33 @@ import ChatBubble from '../ChatBubble/ChatBubble';
 
 import useScrollIntoView from '../../hooks/useScrollIntoView';
 import { UserContext } from '../../contexts/user.context';
+import { LayoutContext } from '../../contexts/layout.context';
 
 import useStyles from './ChatBox.style';
 
-function ChatBox({chatId}) {
+function ChatBox() {
 	const classes = useStyles();
 	const user = useContext(UserContext);
+	const { chatBoxId } = useContext(LayoutContext);
 	const [scrollTargetRef] = useScrollIntoView('instant');
+	const chatObject = chatBoxId.length > 0 && user.chats.filter(chat => chatBoxId === chat.chatId);
+	const messages = chatBoxId.length > 0 && chatObject[0].messages;
 
-let messages = []
-	
 	return (
 		<Grid item xs={5.7} sm={6.5} md={7} className={classes.chatBox}>
 			<ChatBoxHeader />
 			<section className={classes.msgWrapper}>
-				{chatId && messages.map(msg => {
-					return (
-						<ChatBubble
-							key={Math.random() * 9999}
-							msg={msg.msg}
-							isMe={msg.userId === 1 && true}
-							time={msg.time}
-						/>
-					);
-				})}
+				{chatBoxId &&
+					messages.map(msg => {
+						return (
+							<ChatBubble
+								key={msg.msgId}
+								msg={msg.msg}
+								isMe={msg.userId === user.userId && true}
+								time={msg.timestamp}
+							/>
+						);
+					})}
 				<div ref={scrollTargetRef} id='scrollTargetChatBox' />
 			</section>
 
--- src/components/DataList/DataList.js
+++ src/components/DataList/DataList.js
@@ -2,6 +2,7 @@ import React, { useContext } from 'react';
 import { Grid } from '@mui/material';
 
 import { UserContext } from '../../contexts/user.context';
+import { LayoutContext } from '../../contexts/layout.context';
 
 import SearchForm from '../SearchForm';
 import SortForm from '../SortForm/SortForm';
@@ -11,23 +12,40 @@ import useStyles from './DataList.style';
 
 function DataList() {
 	const user = useContext(UserContext);
+	const { dataListContent } = useContext(LayoutContext);
 	const classes = useStyles();
+	const hasData = user && dataListContent ? true : false;
 
 	return (
 		<Grid item sm={4} md={4} className={classes.dataList}>
 			<SearchForm className={classes.dataListSearchForm} />
-			<SortForm />
-			<div className={classes.inboxMessages}>
-				{user &&
-					user.chats.map(chat => (
-						<DataCard
-							key={chat.chatId}
-							userId={chat.members.filter(member => member !== user.userId).toString()}
-							msg={chat.messages[0].msg}
-							time={chat.messages[0].timestamp}
-						/>
-					))}
-			</div>
+			{dataListContent === 'inbox' && <SortForm />}
+
+			{hasData && (
+				<div className={classes.dataListCards}>
+					{dataListContent === 'inbox'
+						? user.chats.map(chat => (
+								<DataCard
+									key={chat.chatId}
+									chatId={chat.chatId}
+									userId={chat.members
+										.filter(member => member !== user.userId)
+										.toString()}
+									msg={chat.messages[0].msg}
+									time={chat.messages[0].timestamp}
+								/>
+						  ))
+						: dataListContent === 'contacts'
+						? user.contacts.map(contact => (
+								<DataCard
+									key={contact.login.uuid}
+									userId={contact.login.uuid}
+									msg={'Offline'}
+								/>
+						  ))
+						: null}
+				</div>
+			)}
 		</Grid>
 	);
 }
