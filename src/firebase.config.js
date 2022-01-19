7c1ef36ce3a70dbc1cdb7597f643203074294c8e
Patrick Pavliuchik <73493957+Pascha-404@users.noreply.github.com>
2022-01-19 20:33:02 +0100

    add firebaseConfig file with .env variables

---
+++ src/firebase.config.js
@@ -0,0 +1,21 @@
+import { initializeApp } from 'firebase/app';
+import { getDatabase } from 'firebase/database';
+
+const firebaseConfig = {
+	apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+	authDomain: process.env.REACT_APP_FIREBASE_PROJECT_ID + '.firebaseapp.com',
+	databaseURL:
+		'https://process.env.' +
+		REACT_APP_FIREBASE_PROJECT_ID +
+		'-default-rtdb.europe-west1.firebasedatabase.app',
+	projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+	storageBucket: process.env.REACT_APP_FIREBASE_PROJECT_ID + '.appspot.com',
+	messagingSenderId: process.env.MSG_SENDER_ID,
+	appId: REACT_APP_FIREBASE_APP_ID,
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+// Get ref to database service
+const database = getDatabase(app);
