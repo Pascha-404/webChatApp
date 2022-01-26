799caa13dc9e07e4cc423474181fe97252f437c2
Patrick Pavliuchik <73493957+Pascha-404@users.noreply.github.com>
2022-01-26 20:47:25 +0100

    add func useUser, show children when fetched

--- src/contexts/user.context.js
+++ src/contexts/user.context.js
@@ -1,11 +1,35 @@
-import { createContext } from 'react';
+import { createContext, useContext } from 'react';
 import useDefaultUser from '../hooks/useDefaultUser';
+import useChatReducer from '../hooks/useChatReducer';
+import chatReducer from '../reducers/chat.reducer';
+import useFetchDatabase from '../services/api/useFetchDatabase';
+import fetchDatabase from '../utilities/fetchDatabase';
+import Loading from '../components/Loading';
 
 const UserContext = createContext();
+const ChatContext = createContext();
+const ChatDispatch = createContext();
 
 function UserProvider({ children }) {
-	const [defaultUser] = useDefaultUser();
-	return <UserContext.Provider value={defaultUser}>{children}</UserContext.Provider>;
+	const [fetchedUser, isFetching] = useFetchDatabase('/users/admin');
+	const [chats, chatDispatch] = useChatReducer(chatReducer);
+
+	return (
+		<UserContext.Provider value={fetchedUser}>
+			<ChatContext.Provider value={chats}>
+				<ChatDispatch.Provider value={chatDispatch}>
+					{!isFetching && children} {isFetching && <Loading />}
+				</ChatDispatch.Provider>
+			</ChatContext.Provider>
+		</UserContext.Provider>
+	);
 }
 
-export { UserContext, UserProvider };
+function useUser() {
+	const context = useContext(UserContext);
+	if (context === undefined) {
+		throw new Error('useUser must be used within a UserProvider');
+	}
+	return context;
+}
+export { useUser, UserProvider, ChatDispatch };
