8dc97fc8ee95185cece33c279a1532c6f76296a1
Patrick Pavliuchik <73493957+Pascha-404@users.noreply.github.com>
2022-01-28 20:18:21 +0100

    add hook for fetching db and return state

---
+++ src/services/api/useFetchDatabase.js
@@ -0,0 +1,26 @@
+import { useState, useEffect } from 'react';
+import database from '../../firebase.config';
+import { ref, onValue } from 'firebase/database';
+
+const useFetchDatabase = path => {
+	const [state, setState] = useState({});
+	const [isFetching, setIsFetching] = useState(true);
+	useEffect(() => {
+		const fetchData = () => {
+			setIsFetching(true)
+			const fetchRef = ref(database, path);
+
+			onValue(fetchRef, snapshot => {
+				const data = snapshot.val();
+				setState(data);
+				setIsFetching(false)
+			});
+			
+        };
+        fetchData()
+	}, [path]);
+
+	return [state, isFetching];
+};
+
+export default useFetchDatabase;
