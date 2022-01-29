a9cf5324869ae7787fe468af4979076b1badf7c9
Patrick Pavliuchik <73493957+Pascha-404@users.noreply.github.com>
2022-01-29 19:33:40 +0100

    first version of a dispatch for chatReducer

---
+++ src/reducers/chat.reducer.js
@@ -0,0 +1,36 @@
+import { v4 as uuid } from 'uuid';
+
+const reducer = (state, action) => {
+	switch (action.type) {
+		case 'SET_STATE':
+			return action.state
+		case 'DELETE_CHAT':
+			return [state.filter(chat => action.chatId !== chat.chatId)];
+		case 'CREATE_CHAT':
+			return [
+				...state,
+				{
+					chatId: uuid(),
+					members: [action.userId, action.contactId],
+					messages: [],
+					chatCreated: Date.now(),
+					lastMsgSend: Date.now(),
+				},
+			];
+		case 'ADD_MSG':
+			const msg = {
+				msg: action.msg,
+				msgId: uuid(),
+				timestamp: Date.now,
+				userId: action.userId,
+			};
+			const chat = state.filter(chat => action.chatId === chat.chatId);
+			chat.messages.push(msg);
+			return [state.filter(chat => action.chatId !== chat.chatId), chat];
+
+		default:
+			return state;
+	}
+};
+
+export default reducer;
