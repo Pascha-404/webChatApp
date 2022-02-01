6895af49e2d6aca67f858e984c445c576946c00b
Patrick Pavliuchik <73493957+Pascha-404@users.noreply.github.com>
2022-02-01 13:53:56 +0100

    add chatFetch and generating content based on that

--- src/components/ChatBox/ChatBox.js
+++ src/components/ChatBox/ChatBox.js
@@ -1,8 +1,10 @@
-import React, { useContext } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Grid } from '@mui/material';
 import ChatBoxInput from '../ChatBoxInput';
 import ChatBoxHeader from '../ChatBoxHeader/ChatBoxHeader';
 import ChatBubble from '../ChatBubble/ChatBubble';
+
+import useFetchDatabase from '../../services/api/useFetchDatabase';
 
 import useScrollIntoView from '../../hooks/useScrollIntoView';
 import { useUser } from '../../contexts/user.context';
@@ -12,28 +14,36 @@ import useStyles from './ChatBox.style';
 
 function ChatBox() {
 	const classes = useStyles();
-	const user = useUser();
-	const { chatBoxId } = useLayoutContext();
+	const { uuid } = useUser();
+	const { chatBox } = useLayoutContext();
+	const [fetchedMessages, isFetching] = useFetchDatabase(`/messages/${chatBox.id}`);
 	const [scrollTargetRef] = useScrollIntoView('instant');
-	const chatObject =
-		chatBoxId.length > 0 && user.chats.filter(chat => chatBoxId === chat.chatId);
-	const messages = chatBoxId.length > 0 && chatObject[0].messages;
+	const [generatedContent, setGeneratedContent] = useState();
+
+	useEffect(() => {
+		if (!isFetching && fetchedMessages !== null) {
+			const msgBubbles = fetchedMessages.map(msgObject => {
+				const { msgId, msg, sentBy, timestamp } = msgObject[Object.keys(msgObject)[0]];
+				return (
+					<ChatBubble
+						key={msgId}
+						msg={msg}
+						isMe={sentBy === uuid && true}
+						time={timestamp}
+					/>
+				);
+			});
+			setGeneratedContent(msgBubbles);
+		} else if (!isFetching && fetchedMessages === null) {
+			setGeneratedContent('');
+		}
+	}, [fetchedMessages]);
 
 	return (
 		<Grid item xs={5.7} sm={6.5} md={7} className={classes.chatBox}>
 			<ChatBoxHeader />
 			<section className={classes.msgWrapper}>
-				{chatBoxId &&
-					messages.map(msg => {
-						return (
-							<ChatBubble
-								key={msg.msgId}
-								msg={msg.msg}
-								isMe={msg.userId === user.userId && true}
-								time={msg.timestamp}
-							/>
-						);
-					})}
+				{generatedContent}
 				<div ref={scrollTargetRef} id='scrollTargetChatBox' />
 			</section>
 
