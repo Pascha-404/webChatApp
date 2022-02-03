7e9d8d7d1cabaf3ce8b290f2cbc271182db0923b
Patrick Pavliuchik <73493957+Pascha-404@users.noreply.github.com>
2022-02-03 23:41:23 +0100

    add func for updating chat with msg

---
+++ src/services/api/addDatabaseMessage.js
@@ -0,0 +1,28 @@
+import database from '../../firebase.config';
+import { ref, push, update, child } from 'firebase/database';
+
+function addDatabaseMessage(paramsObj) {
+	// Get a key for a new Post.
+	const newMsgKey = push(child(ref(database), '/messages/' + paramsObj.chatId)).key;
+
+	const messageData = {
+		sentBy: paramsObj.sentBy,
+		chatId: paramsObj.chatId,
+		msg: paramsObj.msg,
+		timestamp: Date.now(),
+		msgId: newMsgKey,
+	};
+
+	// Write the new post's data simultaneously in the posts list and the user's post list.
+	const updates = {};
+	updates[`/messages/${paramsObj.chatId}/${newMsgKey}`] = messageData;
+	updates[`/userChats/${paramsObj.chatId}/MsgTimestamp`] =
+		messageData.timestamp;
+	updates[`/userChats/${paramsObj.chatId}/lastMsg`] = {
+		[messageData.sentBy]: messageData.msg,
+	};
+
+	return update(ref(database), updates);
+}
+
+export default addDatabaseMessage;
