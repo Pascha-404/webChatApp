c0b320cc8e3f902effc28469ff03150277a080f8
Patrick Pavliuchik <73493957+Pascha-404@users.noreply.github.com>
2022-02-04 15:17:31 +0100

    delete unused files

--- src/hooks/useLayoutReducer.js
+++
@@ -1,9 +0,0 @@
-import { useReducer } from 'react';
-
-const useLayoutReducer = (reducer, defaultValue) => {
-	const [state, dispatch] = useReducer(reducer, defaultValue);
-
-	return [state, dispatch];
-};
-
-export default useLayoutReducer;
