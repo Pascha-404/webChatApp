f2ab9445bcc1d7fefd4353844347f84b19550c57
Patrick Pavliuchik <73493957+Pascha-404@users.noreply.github.com>
2022-02-06 21:13:13 +0100

    restructure code, use context instead of fetching

--- src/components/DataList/DataList.js
+++ src/components/DataList/DataList.js
@@ -1,10 +1,10 @@
 import React, { useState, useEffect } from 'react';
 import { Grid } from '@mui/material';
 
+import { useContacts } from '../../contexts/contacts.context';
 import { useUser } from '../../contexts/user.context';
 import { useChats } from '../../contexts/chats.context';
 import { useLayout } from '../../contexts/layout.context';
-import fetchDatabase from '../../services/api/fetchDatabase';
 
 import SearchForm from '../SearchForm';
 import SortForm from '../SortForm/SortForm';
@@ -15,29 +15,31 @@ import useStyles from './DataList.style';
 function DataList() {
 	const user = useUser();
 	const chats = useChats();
+	const contacts = useContacts();
 	const { dataListContent } = useLayout();
 	const classes = useStyles();
 	const [generatedContent, setGeneratedContent] = useState();
+
 	useEffect(() => {
 		if (dataListContent === 'inbox') {
-			const generatedChats = chats.map(async chat => {
-				const contact = chat.members.filter(member => user.uuid !== member);
-				const contactFetch = await fetchDatabase(`users/${contact}`);
+			const generatedChats = chats.map(chat => {
+				const chatPartner = chat.members.filter(member => user.uuid !== member);
+				const contactData = contacts.filter(
+					contact => String(chatPartner) === contact.uuid
+				);
 				return (
 					<DataCard
 						key={chat.chatId}
 						chatId={chat.chatId}
-						target={contactFetch}
+						target={contactData[0]}
 						msg={chat.lastMsg && chat.lastMsg}
 						time={chat.timestamp && chat.timestamp}
 					/>
 				);
 			});
-			Promise.all(generatedChats)
-				.then(results => setGeneratedContent(results))
-				.catch(error => console.log(error));
+			setGeneratedContent(generatedChats);
 		}
-	}, [chats, dataListContent, user.uuid]);
+	}, [chats, dataListContent, user.uuid, contacts]);
 
 	return (
 		<Grid item sm={4} md={4} className={classes.dataList}>
