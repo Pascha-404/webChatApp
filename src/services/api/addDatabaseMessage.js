4b9b44e58e9e513e434ec11743171f89a3bd4ba7
Patrick Pavliuchik <73493957+Pascha-404@users.noreply.github.com>
2022-02-11 17:42:10 +0100

    fixed naming

--- src/services/api/addDatabaseMessage.js
+++ src/services/api/addDatabaseMessage.js
@@ -9,20 +9,19 @@ function addDatabaseMessage(paramsObj) {
 		sentBy: paramsObj.sentBy,
 		chatId: paramsObj.chatId,
 		msg: paramsObj.msg,
-		timestamp: Date.now(),
+		msgTimestamp: Date.now(),
 		msgId: newMsgKey,
 	};
 
 	// Write the nmessage data simultaneously in the messages of the userChat list and the user's post list.
 	const updates = {};
 	updates[`/messages/${paramsObj.chatId}/${newMsgKey}`] = messageData;
-	updates[`/userChats/${paramsObj.chatId}/MsgTimestamp`] =
-		messageData.timestamp;
+	updates[`/userChats/${paramsObj.chatId}/msgTimestamp`] = messageData.msgTimestamp;
 	updates[`/userChats/${paramsObj.chatId}/lastMsg`] = {
 		[messageData.sentBy]: messageData.msg,
 	};
-update(ref(database), updates);
-	return messageData
+	update(ref(database), updates);
+	return messageData;
 }
 
 export default addDatabaseMessage;
