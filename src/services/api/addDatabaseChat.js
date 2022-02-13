62085f83d4d67302aef83efb1eea0881c216c258
Patrick Pavliuchik <73493957+Pascha-404@users.noreply.github.com>
2022-02-13 22:04:15 +0100

    add function for adding new chats

---
+++ src/services/api/addDatabaseChat.js
@@ -0,0 +1,25 @@
+import database from '../../firebase.config';
+import { ref, push, update, child } from 'firebase/database';
+
+function addDatabaseChat(paramsObj) {
+	// Get a key for a new Post.
+	const newChatKey = push(child(ref(database), '/userChats')).key;
+
+	const chatData = {
+		chatId: newChatKey,
+		chatTimestamp: Date.now(),
+		lastMsg: {},
+		members: [paramsObj.user, paramsObj.target],
+		msgTimestamp: null,
+	};
+
+	// Write the nmessage data simultaneously in the messages of the userChat list and the user's post list.
+	const updates = {};
+    updates[`/userChats/${newChatKey}`] = chatData;
+    updates[`/users/${paramsObj.user}/userChats/${newChatKey}`] = true;
+	
+	update(ref(database), updates);
+	return { chatData, newChatKey };
+}
+
+export default addDatabaseChat;
