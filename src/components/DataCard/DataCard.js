c8b376bf38ed2a19ff073c94355d5ae48e99cf8b
Patrick Pavliuchik <73493957+Pascha-404@users.noreply.github.com>
2022-02-13 22:05:07 +0100

    add condition for click depending on type of card

--- src/components/DataCard/DataCard.js
+++ src/components/DataCard/DataCard.js
@@ -5,18 +5,33 @@ import UserAvatar from '../UserAvatar';
 import useStyles from './DataCard.style';
 
 import { useLayoutDispatch } from '../../contexts/layout.context';
+import { useChats, useChatsDispatch } from '../../contexts/chats.context';
+import { useUser } from '../../contexts/user.context';
 
-function DataCard({ target, time, msg, chatId }) {
+function DataCard({ target, time, msg, chatId, type }) {
 	const classes = useStyles();
+	const user = useUser();
+	const chats = useChats();
+	const chatsDispatch = useChatsDispatch();
 	const layoutDispatch = useLayoutDispatch();
 	const targetName = target.firstName + ' ' + target.lastName;
-	
+
+	function handleClick() {
+		if (type === 'chat') {
+			layoutDispatch({ type: 'SET_CHATBOX', id: chatId, target: target.uuid });
+		} else if (type === 'contact') {
+			const checkChats = chats.filter(chat => chat.members.includes(target.uuid));
+			if (checkChats.length === 1) {
+				layoutDispatch({ type: 'SET_CHATBOX', id: checkChats[0].chatId, target: target.uuid });
+			} else if (checkChats.length === 0) {
+				chatsDispatch({ type: 'CREATE_CHAT', user: user.uuid, target: target.uuid });
+				
+			}
+		}
+	}
+
 	return (
-		<Card
-			className={classes.dataCard}
-			onClick={() =>
-				layoutDispatch({ type: 'SET_CHATBOX', id: chatId, target: target.uuid })
-			}>
+		<Card className={classes.dataCard} onClick={handleClick}>
 			<CardHeader
 				avatar={<UserAvatar userName={targetName} imgUrl={target.pictureLink} />}
 				title={targetName}
