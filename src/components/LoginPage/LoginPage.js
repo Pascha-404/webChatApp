6fd7b3096b9121a9c09f537960e137b02974ba72
Patrick Pavliuchik <73493957+Pascha-404@users.noreply.github.com>
2022-03-07 20:51:44 +0100

    link LoginForm

---
+++ src/components/LoginPage/LoginPage.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import PageContent from '../layout/PageContent'
+import LoginForm from '../LoginForm/LoginForm';
+
+function LoginPage() {
+  
+  return (
+		<PageContent>
+        <LoginForm />
+		</PageContent>
+	);
+}
+
+export default LoginPage
