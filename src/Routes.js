45c9ce47c338c1292cf5d51a65698286ad543967
Patrick Pavliuchik <73493957+Pascha-404@users.noreply.github.com>
2022-03-13 17:31:26 +0100

    add auth/register routes to app

--- src/Routes.js
+++ src/Routes.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import { AuthProvider } from './contexts/auth.context';
 import { Routes, Route } from 'react-router';
-import LoginPage from './components/LoginPage/LoginPage';
+import AuthPage from './components/AuthPage/AuthPage';
 import WebChatApp from './components/WebChatApp';
 import RequireAuth from './components/RequireAuth/RequireAuth';
+import AuthForm from './components/AuthForm/AuthForm';
 
 function Routing() {
 	return (
@@ -17,7 +18,12 @@ function Routing() {
 						</RequireAuth>
 					}
 				/>
-				<Route path='login' element={<LoginPage />} />
+				<Route path='auth' element={<AuthPage />}>
+					<Route index element={<AuthForm formState='authType' />} />
+					<Route path='email' element={<AuthForm formState='authForm' />}>
+						<Route path='register' element={<AuthForm formState='registerForm' />} />{' '}
+					</Route>
+				</Route>
 			</Routes>
 		</AuthProvider>
 	);
