f447a66c13c2239a62290d71a80d3e4d774e1904
Patrick Pavliuchik <73493957+Pascha-404@users.noreply.github.com>
2022-03-16 19:59:29 +0100

    create starter files and starter code for optionsComp

---
+++ src/components/Options/Options.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import useStyles from './Options.style';
+
+function Options() {
+	const classes = useStyles();
+	return <div>Options</div>;
+}
+
+export default Options;
