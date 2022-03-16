cedb38bfec39ff06f617b844f37bd836ca287478
Patrick Pavliuchik <73493957+Pascha-404@users.noreply.github.com>
2022-03-16 14:58:47 +0100

    add reducer to authContext and first code to anonymSwitchCase

--- src/contexts/auth.context.js
+++ src/contexts/auth.context.js
@@ -1,28 +1,30 @@
-import React, { createContext, useContext, useState } from 'react';
+import React, { createContext, useContext } from 'react';
+import useAuthReducer from '../hooks/useAuthReducer';
+import authReducer from '../reducers/auth.reducer.js';
 
 const AuthContext = createContext();
 const AuthDispatch = createContext();
 
 function useAuth() {
-    const context = useContext(AuthContext);
-    if (context === undefined) {
-        throw new Error('useAuth must be used within a AuthProvider')
-    }
+	const context = useContext(AuthContext);
+	if (context === undefined) {
+		throw new Error('useAuth must be used within a AuthProvider');
+	}
 	return context;
 }
 function useAuthDispatch() {
-    const dispatch = useContext(AuthDispatch);
-    if (dispatch === undefined) {
-        throw new Error('useAuthDispatch must be used within a AuthProvider')
-    }
+	const dispatch = useContext(AuthDispatch);
+	if (dispatch === undefined) {
+		throw new Error('useAuthDispatch must be used within a AuthProvider');
+	}
 	return dispatch;
 }
 
 function AuthProvider({ children }) {
-	let [auth, setauth] = useState('');
+	const [auth, dispatch] = useAuthReducer(authReducer, '');
 	return (
 		<AuthContext.Provider value={auth}>
-			<AuthDispatch.Provider value="dispatch">{children}</AuthDispatch.Provider>
+			<AuthDispatch.Provider value={dispatch}>{children}</AuthDispatch.Provider>
 		</AuthContext.Provider>
 	);
 }
--- src/reducers/auth.reducer.js
+++ src/reducers/auth.reducer.js
@@ -1,17 +1,39 @@
-import { auth } from "../firebase.config";
+import { auth } from '../firebase.config';
+import { signInAnonymously } from 'firebase/auth';
 
 const reducer = (state, action) => {
-    switch (action.type) {
-			case 'AUTH_GOOGLE':
-				break;
-			case 'AUTH_GITHUB':
-				break;
-			case 'AUTH_EMAIL':
-				break;
-			case 'AUTH_ANONYM':
-				break;
+	const userObject = {
+		uuid: '',
+		displayName: '',
+		email: '',
+		emailVerified: false,
+		photoURL: '',
+	};
 
-			default:
-				return state;
-		}
-}
+	switch (action.type) {
+		case 'AUTH_GOOGLE':
+			break;
+		case 'AUTH_GITHUB':
+			break;
+		case 'AUTH_EMAIL':
+			break;
+		case 'AUTH_ANONYM':
+			signInAnonymously(auth)
+				.then(data => {
+					userObject.uuid = data.user.uid;
+					userObject.displayName = data.user.displayName;
+					userObject.email = data.user.email;
+					userObject.emailVerified = data.user.emailVerified;
+					userObject.photoURL = data.user.photoURL;
+				})
+				.catch(error => {
+					console.log(error);
+				});
+			return { userObject };
+
+		default:
+			return state;
+	}
+};
+
+export default reducer;
