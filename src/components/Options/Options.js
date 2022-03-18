ee278b4230f42cb29197dbb78dfcdaac3ff54198
Patrick Pavliuchik <73493957+Pascha-404@users.noreply.github.com>
2022-03-18 16:16:32 +0100

    add Button with signoutHandler, apply btn style from authForm

--- src/components/Options/Options.js
+++ src/components/Options/Options.js
@@ -1,9 +1,30 @@
 import React from 'react';
+import { Button } from '@mui/material';
+
+import { useNavigate } from 'react-router-dom';
+import { useAuthDispatch } from '../../contexts/auth.context';
+
 import useStyles from './Options.style';
 
 function Options() {
+	const authDispatch = useAuthDispatch();
+	const navigate = useNavigate();
 	const classes = useStyles();
-	return <div>Options</div>;
+
+	function handleSignout() {
+		authDispatch({ type: 'SIGNOUT' });
+		navigate('/auth');
+	}
+
+	return (
+		<section className={classes.options}>
+			<Button
+				className={`${classes.interactionField} signoutBtn`}
+				onClick={handleSignout}>
+				Signout
+			</Button>
+		</section>
+	);
 }
 
 export default Options;
