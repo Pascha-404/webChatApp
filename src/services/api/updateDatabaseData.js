602cd6be959f3c20a0a05d7d725c8f2cc286562d
Patrick Pavliuchik <73493957+Pascha-404@users.noreply.github.com>
2022-04-08 21:55:21 +0200

    create updateDatabaseData() file and add it to api dir index

---
+++ src/services/api/updateDatabaseData.js
@@ -0,0 +1,15 @@
+import { database } from '../../firebase.config';
+import { ref, update } from 'firebase/database';
+
+//updateArray example: [{path: string, value: any}]
+function updateDatabaseData(updateArray) {
+	// Loops through every object of given updateArray,
+	// adds it to updates object and sends it to database
+	const updates = {};
+	for (let update of updateArray) {
+		updates[update.path] = update.value;
+	}
+	update(ref(database), updates);
+}
+
+export default updateDatabaseData;
