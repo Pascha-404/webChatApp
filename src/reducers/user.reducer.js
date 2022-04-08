df3b53e74bbb02d232381a51b0efa851f9967f59
Patrick Pavliuchik <73493957+Pascha-404@users.noreply.github.com>
2022-04-08 21:59:31 +0200

    add updateDatabaseData() to switch("ADD_CONTACT")

--- src/reducers/user.reducer.js
+++ src/reducers/user.reducer.js
@@ -1,12 +1,17 @@
+import { updateDatabaseData } from '../services/api';
+
 const reducer = (state, action) => {
 	switch (action.type) {
-        case 'SET_STATE':
-            return action.state;
-        case 'ADD_CONTACT':
-            return {...state, contacts: {...state.contacts, [action.newContact]: true}}
+		case 'SET_STATE':
+			return action.state;
+		case 'ADD_CONTACT':
+			updateDatabaseData([
+				{ path: `/users/${state.uuid}/contacts/${action.newContact.uuid}`, value: true },
+			]);
+			return { ...state, contacts: { ...state.contacts, [action.newContact]: true } };
 		default:
 			return state;
 	}
 };
 
-export default reducer
+export default reducer;
