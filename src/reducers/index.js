a15a97150bd46d900fe5eb27d1b641b6afc56965
Patrick Pavliuchik <73493957+Pascha-404@users.noreply.github.com>
2022-04-15 17:40:52 +0200

    create index file for reducer directory

---
+++ src/reducers/index.js
@@ -0,0 +1,19 @@
+import authReducer from './auth.reducer';
+import chatReducer from './chat.reducer';
+import contactReducer from './contact.reducer';
+import findContactReducer from './findContact.reducer';
+import groupReducer from './group.reducer';
+import layoutReducer from './layout.reducer';
+import messageReducer from './message.reducer';
+import userReducer from './user.reducer';
+
+export {
+	authReducer,
+	chatReducer,
+	contactReducer,
+	findContactReducer,
+	groupReducer,
+	layoutReducer,
+	messageReducer,
+	userReducer,
+};
