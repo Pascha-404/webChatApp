62373d1ac98f6f10504bf4c5a7ccd412ee0d2ff9
Patrick Pavliuchik <73493957+Pascha-404@users.noreply.github.com>
2022-04-15 21:49:58 +0200

    grab GroupsContext and generate content based on that

--- src/components/DataList/DataList.js
+++ src/components/DataList/DataList.js
@@ -5,13 +5,14 @@ import RefreshIcon from '@mui/icons-material/Refresh';
 
 import sortByTimestamp from '../../utilities/sortByTimestamp';
 import {
+	useUser,
+	useChats,
+	useLayout,
 	useContacts,
 	useFindContacts,
 	useFindContactsDispatch,
-} from '../../contexts/contacts.context';
-import { useUser } from '../../contexts/user.context';
-import { useChats } from '../../contexts/chats.context';
-import { useLayout } from '../../contexts/layout.context';
+	useGroups,
+} from '../../contexts';
 
 import SearchForm from '../SearchForm';
 import SortForm from '../SortForm/SortForm';
@@ -24,6 +25,7 @@ import TabBar from '../TabBar';
 function DataList() {
 	const user = useUser();
 	const chats = useChats();
+	const groups = useGroups();
 	const contacts = useContacts();
 	const foundContactsDispatch = useFindContactsDispatch();
 	const { foundContacts } = useFindContacts();
@@ -76,7 +78,12 @@ function DataList() {
 					setGeneratedContent(generatedContacts);
 				}
 			} else if (dataListContent === 'groups') {
-				setGeneratedContent('');
+				if (dataListTab.groups === 'existingGroups') {
+					const generatedGroups = groups.map(group => {
+						return <DataCard type='contact' key={group.uuid} target={group} />;
+					});
+					setGeneratedContent(generatedGroups);
+				}
 			} else if (dataListContent === 'notifications') {
 				setGeneratedContent('');
 			} else if (dataListContent === 'options') {
@@ -84,7 +91,7 @@ function DataList() {
 			}
 		}
 		createContent();
-	}, [chats, dataListContent, user.uuid, contacts, dataListTab, foundContacts]);
+	}, [chats, dataListContent, user.uuid, contacts, dataListTab, foundContacts, groups]);
 
 	return (
 		<Grid item sm={4} md={4} className={classes.dataList}>
