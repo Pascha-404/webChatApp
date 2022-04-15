877259384666cd921c031038f0a72b27b2e32b70
Patrick Pavliuchik <73493957+Pascha-404@users.noreply.github.com>
2022-04-15 21:46:42 +0200

    add logic for context and initial useEffect for fetching group data

---
+++ src/contexts/groups.context.js
@@ -0,0 +1,40 @@
+import React, { createContext, useContext } from 'react';
+
+import useGroupReducer from '../hooks/useGroupReducer';
+import { groupReducer } from '../reducers';
+import { useUser } from './user.context';
+
+import Loading from '../components/Loading';
+
+const GroupsContext = createContext();
+const GroupsDispatch = createContext();
+
+function useGroups() {
+	const context = useContext(GroupsContext);
+	if (context === undefined) {
+		throw new Error('useGroups must be used within a GroupsProvider');
+	}
+	return context;
+}
+
+function useGroupsDispatch() {
+	const dispatch = useContext(GroupsDispatch);
+	if (dispatch === undefined) {
+		throw new Error('useGroupsDispatch must be used within a GroupsProvider');
+	}
+	return dispatch;
+}
+
+function GroupsProvider({ children }) {
+	const { groupChats } = useUser();
+	const [state, dispatch, isFetching] = useGroupReducer(groupReducer, groupChats, []);
+	return (
+		<GroupsContext.Provider value={state}>
+			<GroupsDispatch.Provider value={dispatch}>
+				{isFetching ? <Loading /> : children}
+			</GroupsDispatch.Provider>
+		</GroupsContext.Provider>
+	);
+}
+
+export { GroupsProvider, useGroups, useGroupsDispatch };
---
+++ src/hooks/useGroupReducer.js
@@ -0,0 +1,36 @@
+import { useReducer, useState, useEffect } from 'react';
+import { fetchDatabase } from '../services/api';
+
+const useGroupReducer = (reducer, userGroups, initialValue) => {
+	const [state, dispatch] = useReducer(reducer, initialValue);
+	const [isFetching, setIsFetching] = useState(true);
+
+	useEffect(() => {
+		async function fetchGroupsData() {
+			try {
+				setIsFetching(true);
+				const activeGroups = Object.keys(userGroups).filter(
+					group => userGroups[group] === true
+				);
+				const fetchedData = await Promise.all(
+					activeGroups.map(async group => {
+						const fetchedGroup = await fetchDatabase(`/groups/${group}`);
+						const { displayName, uuid, photoURL, members } = fetchedGroup;
+						const groupObject = { displayName, uuid, photoURL, members, isMember: true };
+						return groupObject;
+					})
+				);
+				dispatch({ type: 'SET_STATE', state: fetchedData });
+				setIsFetching(false);
+			} catch (error) {
+				console.log(error);
+			}
+		}
+		fetchGroupsData();
+	// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, []);
+
+	return [state, dispatch, isFetching];
+};
+
+export default useGroupReducer;
