21a0b5d24f05f6044e166cc6c946fa3eafc21953
Patrick Pavliuchik <73493957+Pascha-404@users.noreply.github.com>
2022-04-27 16:04:41 +0200

    add activeGroups to useEffect dependency to update after joining/leaving a group, set fetching to false after fetch

--- src/hooks/useGroupChatReducer.js
+++ src/hooks/useGroupChatReducer.js
@@ -14,10 +14,11 @@ const useGroupChatReducer = (reducer, activeGroups, setIsFetching, initialValue)
 		Promise.all(fetchedChats)
 			.then(results => {
 				dispatch({ type: 'SET_STATE', state: results });
+				setIsFetching(false)
 			})
 			.catch(error => console.log(error));
 	// eslint-disable-next-line react-hooks/exhaustive-deps
-	}, [dispatch]);
+	}, [dispatch, activeGroups]);
 
 	return [state, dispatch];
 };
