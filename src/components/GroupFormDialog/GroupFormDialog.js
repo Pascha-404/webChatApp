ed2f69f496ec7714423654005035b5bf642e8aec
Patrick Pavliuchik <73493957+Pascha-404@users.noreply.github.com>
2022-05-03 16:21:15 +0200

    add useEffect() for creating list to add members, add first structure for rendering that list

--- src/components/GroupFormDialog/GroupFormDialog.js
+++ src/components/GroupFormDialog/GroupFormDialog.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import {
 	Dialog,
 	DialogTitle,
@@ -7,24 +7,52 @@ import {
 	TextField,
 	DialogActions,
 	Button,
+	FormControl,
+	FormGroup,
+	FormLabel,
+	FormControlLabel,
+	Checkbox,
+	FormHelperText,
 } from '@mui/material';
 
-import { useLayout, useLayoutDispatch } from '../../contexts';
+import { useContacts, useLayout, useLayoutDispatch } from '../../contexts';
 
 function GroupFormDialog() {
 	const { groupDialog } = useLayout();
+	const contacts = useContacts();
 	const layoutDispatch = useLayoutDispatch();
+	const [members, setMembers] = useState([]);
 
 	function handleCloseDialog() {
 		layoutDispatch({ type: 'SHOW_GROUPDIALOG', value: false });
 	}
 
+	function handleChange(e) {
+		setMembers({ ...members, [e.target.name]: e.target.checked });
+	}
+
+	useEffect(() => {
+		let isActive = true;
+		if (isActive) {
+			const createList = contacts
+				.filter(contact => contact.isFriend)
+				.map(contact => {
+					const { displayName, photoURL, uuid } = contact;
+					return { displayName, photoURL, uuid, isChecked: false };
+				});
+			setMembers(createList);
+		}
+		return () => {
+			isActive = false;
+		};
+	}, []);
+
 	return (
 		<Dialog open={groupDialog} onClose={handleCloseDialog}>
-			<DialogTitle>Create a group</DialogTitle>
+			<DialogTitle>Create a Group</DialogTitle>
 			<DialogContent>
 				<DialogContentText>
-					To create a group, please fill out the form. The groupname must be unique.
+					To create a group, please enter a name. The groupname must be unique.
 				</DialogContentText>
 				<TextField
 					autoFocus
@@ -35,6 +63,15 @@ function GroupFormDialog() {
 					fullWidth
 					variant='standard'
 				/>
+				<FormControl sx={{ m: 3 }} component='fieldset' variant='standard'>
+					<FormLabel component='legend'>Add Members</FormLabel>
+          <FormGroup>
+            {members.map(member => <FormControlLabel key={member.uuid}
+							control={<Checkbox checked={member.isChecked} onChange={handleChange} name={member.uuid} />}
+							label={member.displayName}
+						/>)}
+					</FormGroup>
+				</FormControl>
 			</DialogContent>
 			<DialogActions>
 				<Button onClick={handleCloseDialog}>Cancel</Button>
