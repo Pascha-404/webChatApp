a7176fbd728cb5a32e55efe26651d8aa03b68122
Patrick Pavliuchik <73493957+Pascha-404@users.noreply.github.com>
2022-05-11 20:26:06 +0200

    add formElement and make handleSubmit work, reset everything after submit was handled

--- src/components/GroupFormDialog/GroupFormDialog.js
+++ src/components/GroupFormDialog/GroupFormDialog.js
@@ -17,92 +17,118 @@ import {
 
 import UserAvatar from '../UserAvatar';
 
-import { useContacts, useLayout, useLayoutDispatch } from '../../contexts';
+import { useContacts, useGroupsDispatch, useLayout, useLayoutDispatch, useUser } from '../../contexts';
+import useInputState from '../../hooks/useInputState';
+import addDatabaseGroup from '../../services/api/addDatabaseGroup';
 
 function GroupFormDialog() {
 	const { groupDialog } = useLayout();
-	const contacts = useContacts();
+	const { uuid } = useUser();
+  const contacts = useContacts();
+  const groupsDispatch = useGroupsDispatch();
 	const layoutDispatch = useLayoutDispatch();
 	const [members, setMembers] = useState([]);
+	const [getMembers, setGetMembers] = useState(true);
+	const { state, handleChange, reset } = useInputState('');
 
 	function handleCloseDialog() {
 		layoutDispatch({ type: 'SHOW_GROUPDIALOG', value: false });
 	}
 
-	const handleChange = targetedMember => e => {
+	const handleChangeChecked = targetedMember => e => {
 		const memberIndex = members.findIndex(member => member.uuid === targetedMember.uuid);
 		let modifiedMembers = members;
 		modifiedMembers[memberIndex].isChecked = e.target.checked;
 		setMembers([...modifiedMembers]);
 	};
 
+	function handleSubmit(e) {
+		e.preventDefault();
+		const addedMembers = members.filter(member => member.isChecked);
+		const groupData = addDatabaseGroup({
+			userId: uuid,
+			members: addedMembers,
+			groupName: state,
+    });
+    groupsDispatch({type: 'JOIN_GROUP', newGroup: groupData})
+		reset();
+		setGetMembers(true);
+		handleCloseDialog();
+	}
+
 	useEffect(() => {
-		let isActive = true;
-		if (isActive) {
+		if (getMembers) {
 			const createList = contacts
 				.filter(contact => contact.isFriend)
 				.map(contact => {
 					const { displayName, photoURL, uuid } = contact;
 					return { displayName, photoURL, uuid, isChecked: false };
 				});
-			setMembers(createList);
+			setMembers([...createList]);
 		}
 		return () => {
-			isActive = false;
+			setGetMembers(false);
 		};
-	}, []);
+	}, [getMembers]);
 
 	return (
 		<Dialog open={groupDialog} onClose={handleCloseDialog}>
-			<DialogTitle>Create a Group</DialogTitle>
-			<DialogContent>
-				<DialogContentText>
-					To create a group, please enter a name. The groupname must be unique.
-				</DialogContentText>
-				<TextField
-					autoFocus
-					margin='dense'
-					id='name'
-					label='Group Name'
-					type='text'
-					fullWidth
-					variant='standard'
-				/>
-				<FormControl sx={{ m: 3 }} component='fieldset' variant='standard'>
-					<FormLabel component='legend'>Add Members</FormLabel>
-					<FormGroup>
-						{members.map(member => (
-							<FormControlLabel
-								key={member.uuid}
-								control={
-									<div
-										style={{
-											display: 'flex',
-											justifyContent: 'center',
-											alignItems: 'center',
-										}}>
-										<Checkbox
-											checked={member.isChecked}
-											onChange={handleChange(member)}
-											name={member.uuid}
-										/>
-										<UserAvatar
-											size={2}
-											userName={member.displayName}
-											photoURL={member.photoURL}
-										/>
-									</div>
-								}
-								label={member.displayName}
-							/>
-						))}
-					</FormGroup>
-				</FormControl>
-			</DialogContent>
-			<DialogActions>
-				<Button onClick={handleCloseDialog}>Cancel</Button>
-				<Button onClick={handleCloseDialog}>Submit</Button>
-			</DialogActions>
+			<form onSubmit={handleSubmit}>
+				<DialogTitle>Create a Group</DialogTitle>
+				<DialogContent>
+					<DialogContentText>
+						To create a group, please enter a name. The groupname must be unique.
+					</DialogContentText>
+					<TextField
+						required
+						autoFocus
+						margin='dense'
+						id='name'
+						aria-label='Group Name'
+						label='Group Name'
+						type='text'
+						fullWidth
+						variant='standard'
+						value={state}
+						onChange={handleChange}
+					/>
+					<FormControl sx={{ m: 3 }} component='fieldset' variant='standard'>
+						<FormLabel component='legend'>Add Members</FormLabel>
+
+						<FormGroup>
+							{members.map(member => (
+								<FormControlLabel
+									key={member.uuid}
+									control={
+										<div
+											style={{
+												display: 'flex',
+												justifyContent: 'center',
+												alignItems: 'center',
+											}}>
+											<Checkbox
+												checked={member.isChecked}
+												onChange={handleChangeChecked(member)}
+												name={member.uuid}
+											/>
+											<UserAvatar
+												size={2}
+												userName={member.displayName}
+												photoURL={member.photoURL}
+											/>
+										</div>
+									}
+									label={member.displayName}
+								/>
+							))}
+						</FormGroup>
+					</FormControl>
+				</DialogContent>
+				<DialogActions>
+					<Button onClick={handleCloseDialog}>Cancel</Button>
+					<Button type='submit'>Submit</Button>
+				</DialogActions>
+			</form>
 		</Dialog>
 	);
 }
