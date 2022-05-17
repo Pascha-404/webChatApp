5a643bbc9ced3bc12024379623ac86e145e30c25
Patrick Pavliuchik <73493957+Pascha-404@users.noreply.github.com>
2022-05-17 12:06:51 +0200

    add case for deleting group, update database value isDeleted

--- src/reducers/group.reducer.js
+++ src/reducers/group.reducer.js
@@ -1,3 +1,5 @@
+import { updateDatabaseData } from '../services/api';
+
 const reducer = (state, action) => {
 	switch (action.type) {
 		case 'SET_STATE':
@@ -5,10 +7,16 @@ const reducer = (state, action) => {
 		case 'LEAVE_GROUP':
 			return state.filter(group => group.uuid !== action.groupId);
 		case 'JOIN_GROUP':
-			return [action.newGroup, ...state]
+			return [action.newGroup, ...state];
+		case 'DELETE_GROUP':
+			updateDatabaseData([
+				{ path: `/groups/${action.group.uuid}/isDeleted`, value: true },
+			]);
+			const filteredGroups = state.filter(group => group.uuid !== action.group.uuid);
+			return [{ ...action.group, isDeleted: true }, ...filteredGroups];
 		default:
 			return state;
 	}
 };
 
-export default reducer
+export default reducer;
