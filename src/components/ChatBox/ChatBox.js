bfe08491856b1a2c8d0d13e8f3a0c6173784a609
Patrick Pavliuchik <73493957+Pascha-404@users.noreply.github.com>
2022-05-21 11:26:03 +0200

    add comments for ChatBox.js file

--- src/components/ChatBox/ChatBox.js
+++ src/components/ChatBox/ChatBox.js
@@ -10,16 +10,26 @@ import { useUser, useLayout, useMessages, useGroups } from '../../contexts';
 
 import useStyles from './ChatBox.style';
 
+/* 
+ChatBox component in the rigth third of main page.
+Contains ChatBoxHeader, ChatBoxBubbles and ChatBoxInput. 
+*/
 function ChatBox() {
 	const { chatBox } = useLayout();
 	const classes = useStyles();
 	const { uuid } = useUser();
 	const messages = useMessages();
 	const groups = useGroups();
+	// scroll on render last message into view
 	const [scrollTargetRef] = useScrollIntoView('instant');
 	const [generatedContent, setGeneratedContent] = useState();
 	const [isDeletedGroup, setIsDeletedGroup] = useState(false);
 
+	/* 
+	Checks if messages in the message context for the provided chatRoom.
+	If true sort them by timestamp, generate a ChatBubble for each message
+	and set generatedContent. If not leave generatedContent empty.
+	*/
 	useEffect(() => {
 		if (messages !== null) {
 			const msgBubbles = Object.keys(messages)
@@ -41,6 +51,11 @@ function ChatBox() {
 		}
 	}, [messages, uuid]);
 
+	/* 
+	Checks if targeted chatRoom is a groupChat and if the group is deleted.
+	If true disables the ChatBoxInput(adds disabled prop) and displays
+	a message that the group got deleted.
+	*/
 	useEffect(() => {
 		if (chatBox.targetType === 'groupChat') {
 			const targetGroup = groups.filter(group => group.uuid === chatBox.target);
@@ -54,6 +69,7 @@ function ChatBox() {
 		}
 	}, [chatBox.id, chatBox.targetType]);
 
+	// Display Chatbox just if layoutContext of ChatBox has a chatId.
 	if (chatBox.id) {
 		return (
 			<Grid item xs={5.7} sm={6.5} md={7} className={classes.chatBox}>
