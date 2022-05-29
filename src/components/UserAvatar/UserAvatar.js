249cb3b0ddf3e839ffaf6d020377025f9ab0a388
Patrick Pavliuchik <73493957+Pascha-404@users.noreply.github.com>
2022-05-29 22:49:52 +0200

    add comments for UserAvatar.js file

--- src/components/UserAvatar/UserAvatar.js
+++ src/components/UserAvatar/UserAvatar.js
@@ -3,6 +3,13 @@ import Avatar from '@mui/material/Avatar';
 
 import useStyles from './UserAvatar.styles';
 
+/* 
+UserAvatar Component to display the user/group picture.
+Takes 3 properties:
+Size is by default 3rem but can be changed to any wiched rem size.
+PhotoURL should contain a link to the uploaded photo.
+UserName fills the alt attribute of the Avatar Component for accessibility.
+*/
 function UserAvatar({ size = 3, photoURL, userName }) {
 	const classes = useStyles({ size });
 	return (
