71b8ce3c11ea068cbbbb4b04ec19689e1a521410
Patrick Pavliuchik <73493957+Pascha-404@users.noreply.github.com>
2022-06-01 19:18:42 +0200

    add comments for useFindContactReducer.js file

--- src/hooks/useFindContactReducer.js
+++ src/hooks/useFindContactReducer.js
@@ -4,6 +4,13 @@ import { fetchDatabase } from '../services/api';
 const useFindContactReducer = (reducer, knownContacts, uuid, initialValue) => {
 	const [state, dispatch] = useReducer(reducer, initialValue);
 
+	/* 
+	findNewContacts first fetches all users from the database.
+	Then it creates a contactObj from every user that is not the currently
+	authenticated user and pushes it to the empty contactsArray.
+	As next step the contactsArray gets filtered by contacts the user is already friends with.
+	Finally the state is set.
+	*/
 	useEffect(() => {
 		async function findNewContacts(currentUserId) {
 			let contactsArray = [];
