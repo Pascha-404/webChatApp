841769c1051ce62877c4ca00ffc1c053da7b64a1
Patrick Pavliuchik <73493957+Pascha-404@users.noreply.github.com>
2022-06-07 23:12:13 +0200

    add comments for useLocalStorageReducer.js file

--- src/services/localStorage/useLocalStorageReducer.js
+++ src/services/localStorage/useLocalStorageReducer.js
@@ -1,5 +1,11 @@
 import { useEffect, useReducer } from 'react';
 
+/* 
+Reducer method of using localstorage.
+Needs a key under which the data is saved in localstorage and a default value.
+Checks if localstorage key already exists and sets that data as state.
+If false uses default value as state.
+*/
 const useLocalStorageReducer = (reducer, defaultValue, key) => {
 	const [state, dispatch] = useReducer(reducer, defaultValue, () => {
 		let value;
@@ -12,6 +18,7 @@ const useLocalStorageReducer = (reducer, defaultValue, key) => {
 		return value;
 	});
 
+	// On change of key or state saves the current state in localstorage.
 	useEffect(() => {
 		localStorage.setItem(key, JSON.stringify(state));
 	}, [state, key]);
